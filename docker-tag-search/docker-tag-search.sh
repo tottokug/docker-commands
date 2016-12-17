67ed71798ee9edf9a2c9faefb1d01f7e037e96f8
Ryuichi Tokugami <magcot.com@gmail.com>
2016-12-17 22:22:28 +0900

    Command ListにVersionも追加

--- docker-tag-search/docker-tag-search.sh
+++ docker-tag-search/docker-tag-search.sh
@@ -5,11 +5,12 @@ HELP=0
 
 if [ ${HELP} -eq 1 ];
 then
-  echo "USAGE: docker-tag-search TERM"
+  echo "USAGE: docker-tag-search IMAGE"
   exit
 fi
-TERM=$1
+
+IMAGE=$1
 ENDPOINT="https://registry.hub.docker.com"
-curl ${ENDPOINT}/v2/repositories/${TERM}/tags/ -O - |jq .
+curl -s ${ENDPOINT}/v2/repositories/${IMAGE}/tags/  |jq .
 
 # https://registry.hub.docker.com/v2/repositories/microsoft/cntk/tags/
